a6a7b7fbacbaf4d058b4e292605f1b24595a4bec
JT <you@example.com>
2019-06-05 21:38:26 +0100

    randomise colour selection

--- 17_colorGameProject/colorGame.js
+++ 17_colorGameProject/colorGame.js
@@ -1,16 +1,9 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-];
-
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#colorDisp");
 var messageDisplay = document.querySelector("#message")
+var h1 = document.querySelector("h1")
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,6 +16,7 @@ for(var i = 0; i < squares.length; i++){
     squares[i].addEventListener("click", function () {
         var clickedColor = this.style.backgroundColor;
         if (clickedColor === pickedColor) {
+            h1.style.backgroundColor = clickedColor;
             messageDisplay.textContent = "Correct"
             changeColors(clickedColor);
         } else {
@@ -40,5 +34,21 @@ function changeColors(color) {
 }
 
 function pickColor() {
-    return colors[Math.floor(Math.random() * colors.length)];
+    return colors[Math.floor(Math.random() * colors.length)];  
+}
+
+function generateRandomColors(n) {
+    var arr = [] 
+    for (var i = 0; i < n; i++) {
+        arr.push(randomColor());
+    }
+    return arr;
+}
+
+function randomColor() {
+    var r = Math.floor( Math.random()*256 )
+    var g = Math.floor( Math.random()*256 )
+    var b = Math.floor( Math.random()*256 )
+    // the whitespace in the return string is important elsewhere
+    return `rgb(${r}, ${g}, ${b})`;
 }
