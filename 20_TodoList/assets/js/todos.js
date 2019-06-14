1bcefffc0642d7adef33c7e307c64ef5654a5806
Jonathan Thurgood <jonathanthurgood1989@gmail.com>
2019-06-14 10:15:53 +0100

    can add new items, but they do not have click listners assigned

--- 20_TodoList/assets/js/todos.js
+++ 20_TodoList/assets/js/todos.js
@@ -3,10 +3,22 @@ $("li").click(function(){
     $(this).toggleClass("completed");
 }); 
 
-// Click on X to delete the todo
+// Click on X to delete the Todo
 $("span").click(function(event){
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+// 
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){ // if enter
+        // grab from input
+        var todoText = $(this).val();
+        // set to empty
+        $(this).val('');
+        // create new li and add to ul
+        $("ul").append("<li><span>X</span>"+todoText+"</li>");
+    }
 });
