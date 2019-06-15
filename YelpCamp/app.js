25bd8a5509751ce0eb173f3941398a810cd9a7b7
Jonathan Thurgood <jonathanthurgood1989@gmail.com>
2019-06-15 09:08:14 +0100

    add campgrounds route and template

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -7,6 +7,15 @@ app.get("/", (req,res) => {
     res.render("landing")
 });
 
+app.get("/campgrounds", (req,res) => {
+   var campgrounds = [
+       {name: "campsite", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+       {name: "campsite2", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"}
+    ];
+
+   res.render("campgrounds", {campgrounds: campgrounds});
+});
+
 
 const port = process.env.PORT || 4000;
 
