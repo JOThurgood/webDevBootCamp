81f467b011088dc224ad4991264eac286aace9e5
Jonathan Thurgood <jonathanthurgood1989@gmail.com>
2019-06-17 17:47:59 +0100

    add  mongoDB Atlas to create route

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -10,7 +10,7 @@ const mongoose = require('mongoose')
 
 mongoose.connect('mongodb+srv://'+
     process.env.DB_USER + ':' + process.env.DB_PASS + 
-    '@learningcluster-cldnq.mongodb.net/test?retryWrites=true&w=majority', {
+    '@learningcluster-cldnq.mongodb.net/yelp_camp?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true
 }).then(()=> {
@@ -19,25 +19,28 @@ mongoose.connect('mongodb+srv://'+
     console.log('ERROR:', err.message)
 })
 
-var campgrounds = [
-    {name: "campsite", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
-    {name: "campsite2", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
-    {name: "campsite", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
-    {name: "campsite2", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
-    {name: "campsite", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
-    {name: "campsite2", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
-    {name: "campsite", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
-    {name: "campsite2", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
-    {name: "campsite", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
-    {name: "campsite2", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
-    {name: "campsite", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
-    {name: "campsite2", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
-    {name: "campsite", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
-    {name: "campsite2", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
-    {name: "campsite", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
-    {name: "campsite2", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"}
- ];
+// Schema setup
 
+const campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+const Campground = mongoose.model("Campground", campgroundSchema)
+
+// Use this to create a dummy campsite without using the create form
+// Campground.create(
+//     {
+//         name: "campsite", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"
+//     }, (err, campground) => {
+//         if(err){
+//             console.log(err);
+//         } else {
+//             console.log("newly created campground");
+//             console.log(campground);
+//         }
+//     }
+// );
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -47,7 +50,14 @@ app.get("/", (req,res) => {
 });
 
 app.get("/campgrounds", (req,res) => {
-   res.render("campgrounds", {campgrounds: campgrounds});
+    // get all campgrounds from DB
+    Campground.find({}, (err,allCampgrounds) => {
+        if(err){
+            console.log(err);
+        } else {
+            res.render("campgrounds", {campgrounds: allCampgrounds});
+        }
+    });
 });
 
 app.get("/campgrounds/new", (req,res) => {
@@ -58,8 +68,15 @@ app.post("/campgrounds", (req,res) => {
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name: name, image:image}
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
+    // campgrounds.push(newCampground);
+    // create new and save to DB
+    Campground.create(newCampground, (err, newlyCreated)=> {
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 const port = process.env.PORT || 4000;
