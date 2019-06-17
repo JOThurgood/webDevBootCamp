1777f7c7f242fe6224432e85f1c871d6ad7a01b6
Jonathan Thurgood <jonathanthurgood1989@gmail.com>
2019-06-17 09:48:39 +0100

    add campgrounds/new route

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -2,6 +2,12 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 
+var campgrounds = [
+    {name: "campsite", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+    {name: "campsite2", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"}
+ ];
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
@@ -10,18 +16,19 @@ app.get("/", (req,res) => {
 });
 
 app.get("/campgrounds", (req,res) => {
-   var campgrounds = [
-       {name: "campsite", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
-       {name: "campsite2", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"}
-    ];
-
    res.render("campgrounds", {campgrounds: campgrounds});
 });
 
+app.get("/campgrounds/new", (req,res) => {
+    res.render("new.ejs")
+});
+
 app.post("/campgrounds", (req,res) => {
-    res.send("you hit the post route")
-    // get data from form and add to campground array
-    // redirect back to campgrounds page
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image:image}
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
 });
 
 const port = process.env.PORT || 4000;
