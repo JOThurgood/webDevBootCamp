65ef16e95ed3da3ae4d8da0be0cc1ff5e7644024
JT <you@example.com>
2019-06-18 08:32:41 +0100

    setup schema

--- BlogApp/app.js
+++ BlogApp/app.js
@@ -27,8 +27,21 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-const port = process.env.PORT || 4000;
+// schema setup and model config
+const blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+const Blog = mongoose.model("Blog", blogSchema);
 
+// RESTful routes
+
+
+
+// listen
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log(`listening on ${port}`);
 });
