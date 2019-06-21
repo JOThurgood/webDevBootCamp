647f1dc56319e9ac1a036b9ad41d3c0fe606ccee
JT <you@example.com>
2019-06-21 20:54:03 +0100

    add login

--- 34_authentication/app.js
+++ 34_authentication/app.js
@@ -27,6 +27,7 @@ mongoose.connect('mongodb+srv://'+
 })
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(require("express-session")({
     secret: process.env.MYSECRET,
@@ -37,6 +38,7 @@ app.use(require("express-session")({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -57,7 +59,28 @@ app.get("/register", (req,res) => {
 });
 
 app.post("/register", (req,res) => {
-    res.send("register post route");
+    User.register(new User({username: req.body.username}), req.body.password, (err, user)=> {
+        if(err){
+            console.log(err);
+            res.render('register');
+        } else{
+            passport.authenticate("local")(req, res, () => {
+                res.redirect("/secret");
+            });
+        }
+    });
+});
+
+// Login Routes
+
+app.get("/login", (req,res) => {
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), (req, res) => {
 });
 
 // Listen
