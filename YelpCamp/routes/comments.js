c2ac1e82185260077589ce6f13d5a9a1ee92f7ee
Jonathan Thurgood <jonathanthurgood1989@gmail.com>
2019-06-25 14:45:29 +0100

    add coment delete routes and buttons (no auth)

--- YelpCamp/routes/comments.js
+++ YelpCamp/routes/comments.js
@@ -52,7 +52,17 @@ router.put("/:comment_id", (req,res) => {
         if(err){
             res.redirect("back");
         } else {
-            res.redirect("/campgrounds/"+req.params.id)
+            res.redirect("/campgrounds/"+req.params.id);
+        }
+    });
+});
+
+router.delete("/:comment_id", (req,res) => {
+    Comment.findByIdAndRemove(req.params.comment_id, err => {
+        if(err){
+            res.redirect("back");
+        } else {
+            res.redirect("back");
         }
     });
 });
