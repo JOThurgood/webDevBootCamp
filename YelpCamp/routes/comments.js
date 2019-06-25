e67c9dde76720e5f510932a1b70c14a88aac919c
Jonathan Thurgood <jonathanthurgood1989@gmail.com>
2019-06-25 14:25:42 +0100

    add comment update routes (no auth)

--- YelpCamp/routes/comments.js
+++ YelpCamp/routes/comments.js
@@ -3,7 +3,6 @@ const router = express.Router({mergeParams: true});
 const Campground    = require("../models/campground");
 const Comment       = require("../models/comment");
 
-
 router.get("/new", isLoggedIn, (req,res) => {
     Campground.findById(req.params.id, (err,campground) => {
         if(err){
@@ -37,6 +36,26 @@ router.post("/", isLoggedIn, (req,res) => {
     });
 });
 
+router.get("/:comment_id/edit", (req,res) => {
+    Comment.findById(req.params.comment_id, (err, foundComment) => {
+        if(err) {
+            res.redirect("back");
+        } else {
+            res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+        }
+    })
+    
+});
+
+router.put("/:comment_id", (req,res) => {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
+        if(err){
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/"+req.params.id)
+        }
+    });
+});
 
 // is logged in? middlewear
 
