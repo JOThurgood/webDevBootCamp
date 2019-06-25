52721b7c1d87e5e4ad25e14610d0ed30bb0ea881
Jonathan Thurgood <jonathanthurgood1989@gmail.com>
2019-06-25 14:53:21 +0100

    add auth middlewear apply to update routes

--- YelpCamp/routes/comments.js
+++ YelpCamp/routes/comments.js
@@ -36,7 +36,7 @@ router.post("/", isLoggedIn, (req,res) => {
     });
 });
 
-router.get("/:comment_id/edit", (req,res) => {
+router.get("/:comment_id/edit", checkCommentOwnership, (req,res) => {
     Comment.findById(req.params.comment_id, (err, foundComment) => {
         if(err) {
             res.redirect("back");
@@ -47,7 +47,7 @@ router.get("/:comment_id/edit", (req,res) => {
     
 });
 
-router.put("/:comment_id", (req,res) => {
+router.put("/:comment_id", checkCommentOwnership, (req,res) => {
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
         if(err){
             res.redirect("back");
@@ -67,13 +67,33 @@ router.delete("/:comment_id", (req,res) => {
     });
 });
 
-// is logged in? middlewear
+// middlewear
 
 function isLoggedIn(req, res, next) {
     if(req.isAuthenticated()){
         return next();
     } else {
         res.redirect("/login");
+    }
+}
+
+function checkCommentOwnership (req,res,next) {
+    // logged in?
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, (err, foundComment) => {
+            if(err) {
+                res.redirect("back");
+            } else {
+                // does the user own the comment
+                if (foundComment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect("back");
+                }  
+            }
+        });
+    } else {
+        res.send("You need to be logged in to do that")
     }
 }
 module.exports = router;
